9b3b48c93bea283999949bbe21a48f9572eb732a
Ketan <ketanvj@yahoo.com>
2019-03-10 01:13:41 +0000

    First commit

---
+++ terraformVariables/prim/mainprim.tf
@@ -0,0 +1,10 @@
+variable "image" {
+  description = "container image name"
+#  default = "ghost:latest"
+}
+
+# Download the latest Ghost image
+resource "docker_image" "image_id" {
+  name = "${var.image}"
+}
+
