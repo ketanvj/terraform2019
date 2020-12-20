9cdf959e692d781504604f024e66443af6a2c8b7
Ketan J <ketanvj@yahoo.com>
2020-12-20 12:11:49 +0000

    Updated IP address output

--- consoleOutputs/main.tf
+++ consoleOutputs/main.tf
@@ -6,7 +6,7 @@ resource "docker_image" "image_id" {
 # Start the Container
 resource "docker_container" "container_id" {
   name  = "blog"
-  image = "${docker_image.image_id.latest}"
+  image = docker_image.image_id.latest
   ports {
     internal = "2368"
     external = "80"
@@ -14,14 +14,14 @@ resource "docker_container" "container_id" {
 }
 
 #Output the IP Address of the Container
-output "IP Address" {
-  value = "${docker_container.container_id.ip_address}"
+output "IP_Address" {
+  value = docker_container.container_id.ip_address
 }
 
 output "container_name" {
-  value = "${docker_container.container_id.name}"
+  value = docker_container.container_id.name
 }
 
 output "dockerimage_name" {
-  value = "${docker_image.image_id.name}"
+  value = docker_image.image_id.name
 }
