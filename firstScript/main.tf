95f0b7cb154e75d420fb3a4dd6c01dc8521e5910
ketan J <ketanvj@yahoo.com>
2022-01-29 06:40:16 +0000

    modified for docker version

--- firstScript/main.tf
+++ firstScript/main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "2.16.0"
+    }
+  }
+}
+
 # Download the latest Ghost image
 resource "docker_image" "image_id" {
   name = "ghost:latest"
